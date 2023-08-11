939b6dc6c45a9f961485b0b1cea5ecf1af05745b
kalu jonah chinonso <kaluchinonso18@gmail.com>
2023-08-10 20:02:55 -0700

    This is my first C-Hello World low level program.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main This is my first C-Hello World low level program
+ *return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
