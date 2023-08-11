ddb526c9c91ddcfe90af57ea9bba47fa18c456a6
kalu jonah chinonso <kaluchinonso18@gmail.com>
2023-08-10 20:28:59 -0700

    This is my first C-Hello World low level program.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line with printf function
+ *return: it should end with 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n"); 
+return (0);
+}
